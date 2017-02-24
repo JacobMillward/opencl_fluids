ebf0876d34f6ec53886bcc7a334db41a4d13e362
Jacob Millward <jacob@millward.io>
2017-02-24 09:58:08 +0000

    Add missing nclgl files, and attempt to add files to linker (BROKEN)

---
+++ OpenCL_Fluids/Renderer.cpp
@@ -0,0 +1,69 @@
+#include "Renderer.h"
+
+Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	brickTex = LoadTexture("brick.png");
+	glTextureParameteri(brickTex, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(brickTex, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	staticTex = LoadTexture("noise.png");
+	glTextureParameteri(staticTex, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(staticTex, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+}
+
+Renderer::~Renderer(void)	{
+	glDeleteTextures(1, &brickTex);
+	glDeleteTextures(1, &staticTex);
+}
+
+void	Renderer::RenderScene() {
+	for(vector<RenderObject*>::iterator i = renderObjects.begin(); i != renderObjects.end(); ++i ) {
+		Render(*(*i));
+	}
+}
+
+void	Renderer::Render(const RenderObject &o) {
+	modelMatrix = o.GetWorldTransform();
+
+	if(o.GetShader() && o.GetMesh()) {
+		GLuint program = o.GetShader()->GetShaderProgram();
+		
+		glUseProgram(program);
+		glUniform1f(glGetUniformLocation(program, "time"), time);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, brickTex);
+		glUniform1i(glGetUniformLocation(program, "baseTex"), 0);
+
+		if (currentShader == TYPE_BLEND) {
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, staticTex);
+			glUniform1i(glGetUniformLocation(program, "staticTex"), 1);
+		}		
+
+		UpdateShaderMatrices(program);
+
+		o.Draw();
+	}
+
+	for(vector<RenderObject*>::const_iterator i = o.GetChildren().begin(); i != o.GetChildren().end(); ++i ) {
+		Render(*(*i));
+	}
+}
+
+void	Renderer::UpdateScene(float msec) {
+	time += msec;
+	for(vector<RenderObject*>::iterator i = renderObjects.begin(); i != renderObjects.end(); ++i ) {
+		(*i)->Update(msec);
+	}
+}
+
+GLuint Renderer::LoadTexture(string name)
+{
+	return SOIL_load_OGL_texture(name.c_str(), SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+}
---
+++ OpenCL_Fluids/Renderer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "../nclgl/OGLRenderer.h"
+
+#include "RenderObject.h"
+
+#include <vector>
+
+using std::vector;
+
+class Renderer : public OGLRenderer	{
+public:
+	Renderer(Window &parent);
+	~Renderer(void);
+
+	virtual void	RenderScene();
+
+	virtual void	Render(const RenderObject &o);
+
+	virtual void	UpdateScene(float msec);
+
+	void	AddRenderObject(RenderObject &r) {
+		renderObjects.push_back(&r);
+	}
+	
+	enum ShaderTypes { TYPE_SHRINK, TYPE_FADE, TYPE_BLEND, TYPE_CUBE };
+	ShaderTypes currentShader = TYPE_SHRINK;
+	void	resetTime() { time = 0; }
+protected:
+	GLuint LoadTexture(string name);
+	vector<RenderObject*> renderObjects;
+	GLuint brickTex;
+	GLuint staticTex;
+	float time = 0;
+};
+
