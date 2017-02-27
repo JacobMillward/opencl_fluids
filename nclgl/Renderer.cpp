6ad98870e3536b953a43521230d909ac97740ffd
Jacob Millward <jacob@millward.io>
2017-02-27 16:00:50 +0000

    Remove loading of non-existent textures

--- nclgl/Renderer.cpp
+++ nclgl/Renderer.cpp
@@ -5,14 +5,6 @@ Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	brickTex = LoadTexture("brick.png");
-	glTextureParameteri(brickTex, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(brickTex, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	staticTex = LoadTexture("noise.png");
-	glTextureParameteri(staticTex, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(staticTex, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
 }
 
 Renderer::~Renderer(void)	{
